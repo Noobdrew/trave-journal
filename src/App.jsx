2ac381b4e8cecabdeede0fe2f18c1169ecf41ea2
Ivan Marinski <noobdrew23@gmail.com>
2023-06-20 16:17:51 +0300

    app built

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
+import data from "./data";
 
 function App() {
+  let cards = data.map((item) => {
+    return <Card key={item.id} item={item} />;
+  });
   return (
     <div>
       <Header />
-      <div className="card-conteiner">
-        <Card />
-      </div>
+      <div className="card-conteiner">{cards}</div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,31 @@
 import pin from "../assets/pin.png";
 import japan from "../assets/japan.png";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div>
       <div className="card">
-        <img src={japan} alt="japan" />
+        <img src={props.item.imageUrl} alt="japan" className="location-img" />
         <div className="description">
           <div className="location">
-            <img src={pin} alt="marker" />
-            <h4 className="coutry">Japan</h4>
-            <a href="#" className="map-link">
-              View on Google maps
+            <div className="country-conteiner">
+              <img src={pin} alt="marker" />
+              <h4 className="country">{props.item.location}</h4>
+            </div>
+
+            <a
+              href={props.item.googleMapsUrl}
+              className="map-link"
+              target="blank"
+            >
+              Google maps
             </a>
           </div>
-          <h1 className="title">Mount Fuji</h1>
-          <p className="date">12 jan, 2021 - 24 jan, 2021</p>
-          <p className="description-text">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
+          <h1 className="title">{props.item.title}</h1>
+          <p className="date">
+            {props.item.startDate} - {props.item.endDate}
           </p>
+          <p className="description-text">{props.item.description}</p>
         </div>
       </div>
       <hr />
